7e3650fbba88474fab8b993ab1b743789224532d
Austin Eathorne <austineathorne@gmail.com>
2018-09-26 12:07:04 -0400

    Implement 64 band demo.

--- Assets/Scripts/Audio/AudioPeer.cs
+++ Assets/Scripts/Audio/AudioPeer.cs
@@ -10,22 +10,37 @@ public class AudioPeer : MonoSingleton<AudioPeer>
     public float[] samplesLeft = new float[512];
     public float[] samplesRight = new float[512];
 
-    [Header("Bands")]
+    [Header("8 Bands")]
     private float[] frequencyBands = new float[8];
     private int sampleId = 0;
     private int sampleCount = 0;
     private float averageFrequency = 0.0f;
 
-    [Header("Buffer")]
-    public float minBufferDecrease;
-    public float bufferDecreaseMultiplier;
+    [Header("64 Bands")]
+    private float[] frequencyBands64 = new float[64];
+    private int sampleId64 = 0;
+    private int sampleCount64 = 0;
+    private float averageFrequency64 = 0.0f;
+
+    [Header("Buffer 8 Bands")]
+    public float minBufferDecrease, bufferDecreaseMultiplier;
     private float[] bandBuffer = new float[8];
     private float[] bufferDecrease = new float[8];
 
-    [Header("Useable Values")]
+    [Header("Buffer 64 Bands")]
+    public float minBufferDecrease64, bufferDecreaseMultiplier64;
+    private float[] bandBuffer64 = new float[64];
+    private float[] bufferDecrease64 = new float[64];
+
+    [Header("8 Bands Useable")]
     public float[] frequencyBandHighest = new float[8];
     public float[] frequencyBandNormalized = new float[8];
     public float[] bandBufferNormalized = new float[8];
+
+    [Header("64 Bands Useable")]
+    public float[] frequencyBandHighest64 = new float[64];
+    public float[] frequencyBandNormalized64 = new float[64];
+    public float[] bandBufferNormalized64 = new float[64];
 
     [Header("Amplitude")]
     public float currentAmplitude = 0.0f;
@@ -35,6 +50,7 @@ public class AudioPeer : MonoSingleton<AudioPeer>
 
     [Header("Profile")]
     public float initialHighestFrequency;
+    public float initialHighestFrequency64;
 
     [Header("Channel")]
     public Channel channel;
@@ -59,9 +75,16 @@ public class AudioPeer : MonoSingleton<AudioPeer>
         while (this.isRunning)
         {
             this.StartCoroutine(this.GetSpectrumData());
+
             this.StartCoroutine(this.CreateFrequencyBands());
+            this.StartCoroutine(this.CreateFrequencyBands64());
+
             this.StartCoroutine(this.BufferBands());
+            this.StartCoroutine(this.BufferBands64());
+
             this.StartCoroutine(this.CreateNormalizedBands());
+            this.StartCoroutine(this.CreateNormalizedBands64());
+
             yield return this.StartCoroutine(this.GetAmplitude());
 
             yield return null;
@@ -87,6 +110,7 @@ public class AudioPeer : MonoSingleton<AudioPeer>
 
         yield return null;
     }
+
 
     private IEnumerator CreateFrequencyBands()
     {
@@ -156,6 +180,58 @@ public class AudioPeer : MonoSingleton<AudioPeer>
         yield return null;
     }
 
+    private IEnumerator CreateFrequencyBands64()
+    {
+        this.sampleId64 = 0;
+        this.sampleCount64 = 1;
+
+        int power = 0;
+
+        for (int i = 0; i < 64; i++)
+        {
+            if (i == 16 || i == 32 || i == 48 || i == 56)
+            {
+                power++;
+                this.sampleCount64 = (int)Mathf.Pow(2, power);
+
+                if (power == 3)
+                    this.sampleCount64 -= 2;
+            }
+
+            // Calc average amplitude of all samples in band
+            this.averageFrequency64 = 0.0f;
+            for (int j = 0; j < sampleCount64; j++)
+            {
+                switch (this.channel)
+                {
+                    case Channel.Stereo:
+                        this.averageFrequency64 += ((this.samplesLeft[sampleId64] + this.samplesRight[sampleId64]) * 0.5f) * (sampleId64 + 1);
+                        break;
+
+                    case Channel.Left:
+                        this.averageFrequency64 += this.samplesLeft[sampleId64] * (sampleId64 + 1);
+                        break;
+
+                    case Channel.Right:
+                        this.averageFrequency64 += this.samplesRight[sampleId64] * (sampleId64 + 1);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                sampleId64++;
+            }
+
+            this.averageFrequency64 /= sampleId64;
+
+            this.frequencyBands64[i] = this.averageFrequency64;
+        }
+
+        yield return null;
+    }
+
+
     private IEnumerator BufferBands()
     {
         for (int i = 0; i < 8; i++)
@@ -175,6 +251,26 @@ public class AudioPeer : MonoSingleton<AudioPeer>
         yield return null;
     }
 
+    private IEnumerator BufferBands64()
+    {
+        for (int i = 0; i < 64; i++)
+        {
+            if (frequencyBands64[i] > bandBuffer64[i])
+            {
+                this.bandBuffer64[i] = this.frequencyBands64[i];
+                this.bufferDecrease64[i] = this.minBufferDecrease64;
+            }
+            else if (frequencyBands64[i] < bandBuffer64[i])
+            {
+                this.bandBuffer64[i] -= this.bufferDecrease64[i];
+                this.bufferDecrease64[i] *= this.bufferDecreaseMultiplier64;
+            }
+        }
+
+        yield return null;
+    }
+
+
     private IEnumerator CreateNormalizedBands()
     {
         for (int i = 0; i < 8; i++)
@@ -192,6 +288,25 @@ public class AudioPeer : MonoSingleton<AudioPeer>
 
         yield return null;
     }
+
+    private IEnumerator CreateNormalizedBands64()
+    {
+        for (int i = 0; i < 64; i++)
+        {
+            // Get the highest value for this frequency band
+            if (this.frequencyBands64[i] > this.frequencyBandHighest64[i])
+            {
+                this.frequencyBandHighest64[i] = this.frequencyBands64[i];
+            }
+
+            // Normalize values
+            this.frequencyBandNormalized64[i] = this.frequencyBands64[i] / this.frequencyBandHighest64[i];
+            this.bandBufferNormalized64[i] = this.bandBuffer64[i] / this.frequencyBandHighest64[i];
+        }
+
+        yield return null;
+    }
+
 
     private IEnumerator GetAmplitude()
     {
@@ -225,6 +340,11 @@ public class AudioPeer : MonoSingleton<AudioPeer>
         for (int i = 0; i < 8; i++)
         {
             this.frequencyBandHighest[i] = this.initialHighestFrequency;
+        }
+
+        for (int i = 0; i < 64; i++)
+        {
+            this.frequencyBandHighest64[i] = this.initialHighestFrequency64;
         }
 
         this.currentAmplitude = 0.0f;
---
+++ Assets/Scripts/Visualization/Visualization64Bands.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visualization64Bands : VisualizationBase
+{
+    [Header("Container")]
+    public Transform parentContainer;
+
+    [Header("Scale")]
+    public float minMinScale;
+    public float maxMinScale;
+    public float minScale;
+
+    public float minMaxScale;
+    public float maxMaxScale;
+    public float maxScale;
+
+    public float minScaleMultiplier;
+    public float maxScaleMultiplier;
+    public float scaleMultiplier;
+
+    [Header("Buffer")]
+    public bool isUsingBandBuffers;
+
+    [Header("Cube Positioning")]
+    public float cubeSpacing;
+
+    [Header("Cubes")]
+    public GameObject[] cubeArray = new GameObject[64];
+    public Material[] materialArray = new Material[64];
+
+
+    #region Main
+
+    public override IEnumerator Initialize()
+    {
+        this.isInitialized = true;
+
+        yield return this.StartCoroutine(this.InstantiateCubes());
+
+        this.isInitialized = false;
+
+        yield return null;
+    }
+
+    public override IEnumerator Run()
+    {
+        this.isRunning = true;
+
+        this.parentContainer.gameObject.SetActive(true);
+
+        while (this.isRunning)
+        {
+            yield return this.StartCoroutine(this.ScaleCubes());
+
+            yield return null;
+        }
+
+        yield return null;
+    }
+
+    public override IEnumerator Stop()
+    {
+        this.isRunning = false;
+
+        this.parentContainer.gameObject.SetActive(false);
+
+        yield return null;
+    }
+
+    #endregion
+
+    #region Visualization
+
+    public IEnumerator InstantiateCubes()
+    {
+        float totalLength = (1.0f * 64.0f) + (this.cubeSpacing * 63);
+        float xPosition = -(totalLength * 0.5f);
+        float offset = 1.0f + this.cubeSpacing;
+
+
+        for (int i = 0; i < this.cubeArray.Length; i++)
+        {
+            // Instantiate cube
+            ObjectInstantiater.Instance.StartCoroutine(ObjectInstantiater.Instance.InstantiatePrefab(
+                PrefabType.CubeVerticallyScaling, this.parentContainer, this.parentContainer.position, new Quaternion(), _obj => {
+                    if (_obj != null)
+                    {
+                        this.cubeArray[i] = _obj;
+                        this.materialArray[i] = _obj.GetComponent<MeshRenderer>().material;
+                    }
+                }));
+
+            this.cubeArray[i].transform.localPosition = new Vector3(xPosition, 0, 0);
+            xPosition += offset;
+
+            this.materialArray[i] = this.cubeArray[i].GetComponent<MeshRenderer>().material;
+        }
+
+        yield return null;
+    }
+
+    public IEnumerator ScaleCubes()
+    {
+        float scale = 0.0f;
+
+        for (int i = 0; i < this.cubeArray.Length; i++)
+        {
+            if (this.cubeArray[i] == null)
+                continue;
+
+            if (this.isUsingBandBuffers)
+            {
+                scale = AudioPeer.Instance.bandBufferNormalized64[i] * this.scaleMultiplier < this.minScale ?
+                this.minScale : this.minScale + (AudioPeer.Instance.bandBufferNormalized64[i] * this.scaleMultiplier);
+
+                float color = AudioPeer.Instance.bandBufferNormalized64[i];
+                this.materialArray[i].SetColor("_EmissionColor", new Color(color, color, color));
+            }
+            else
+            {
+                scale = AudioPeer.Instance.frequencyBandNormalized64[i] * this.scaleMultiplier < this.minScale ?
+                this.minScale : this.minScale + (AudioPeer.Instance.frequencyBandNormalized64[i] * this.scaleMultiplier);
+
+                float color = AudioPeer.Instance.frequencyBandNormalized64[i];
+                this.materialArray[i].SetColor("_EmissionColor", new Color(color, 0.0f, 1.0f));
+            }
+
+            scale = scale > this.maxScale ? this.maxScale : scale;
+
+            this.cubeArray[i].transform.localScale = new Vector3(
+                this.cubeArray[i].transform.localScale.x, scale, this.cubeArray[i].transform.localScale.z);
+        }
+
+        yield return null;
+    }
+
+    #endregion
+
+    #region Get/Set Radio Future
+
+    public void SetMinScale(float _scale)
+    {
+        this.minScale = _scale;
+    }
+
+    public void SetMaxScale(float _scale)
+    {
+        this.maxScale = _scale;
+    }
+
+    public void SetScaleMultiplier(float _multiplier)
+    {
+        this.scaleMultiplier = _multiplier;
+    }
+
+    #endregion
+}
